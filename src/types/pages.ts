ca8e1ebf47adc0687847961b49eba44ad8601922
AkhilTheBoss <akhileshwarshriram@gmail.com>
2024-12-04 17:31:43 -0800

    feat: added getPageDiff and getPageFiles API

--- src/types/pages.ts
+++ src/types/pages.ts
@@ -1,172 +1,202 @@
-import { BaseQueryParams, PaginationQueryParams, RequestModeQueryParam } from "./requests";
+import {
+  BaseQueryParams,
+  PaginationQueryParams,
+  RequestModeQueryParam,
+} from "./requests";
 import { ExpertUser, PageSecurity } from "./security";
 
 export type GetPageParams = {
-    format?: 'html' | 'xhtml';
-    revision?: string;
-    include?: 'contents' | 'prevnext';
-    includeDeleted?: boolean;
-    mode?: RequestModeQueryParam
-    exclude?: string;
-} & BaseQueryParams
+  format?: "html" | "xhtml";
+  revision?: string;
+  include?: "contents" | "prevnext";
+  includeDeleted?: boolean;
+  mode?: RequestModeQueryParam;
+  exclude?: string;
+} & BaseQueryParams;
 
 export type GetPageContentsParams = {
-    overview?: boolean;
-    include?: string;
-    reltopath?: string;
-    relto?: number;
-    pageid?: number;
-    includes?: 'overview' | 'tags' | 'overview,tags';
-    format?: 'html' | 'xhtml' | 'text' | 'dekicode';
-    highlight?: string;
-    revision?: string;
-    mode: RequestModeQueryParam;
-    section?: string;
-} & BaseQueryParams
+  overview?: boolean;
+  include?: string;
+  reltopath?: string;
+  relto?: number;
+  pageid?: number;
+  includes?: "overview" | "tags" | "overview,tags";
+  format?: "html" | "xhtml" | "text" | "dekicode";
+  highlight?: string;
+  revision?: string;
+  mode: RequestModeQueryParam;
+  section?: string;
+} & BaseQueryParams;
 
 export type GetPageSubPagesParams = BaseQueryParams & PaginationQueryParams;
 export type GetPageSecurityParams = BaseQueryParams & {
-    export?: boolean;
-}
+  export?: boolean;
+};
+
+export type GetPageDiffParams = {
+  diff?: "combined" | "all";
+  previous?: string;
+  format?: "html" | "xhtml";
+  mode?: RequestModeQueryParam;
+  revision?: string;
+} & BaseQueryParams;
+
+export type GetPageFilesParams = BaseQueryParams & PaginationQueryParams;
 
 export type GetPageResponse = PageBase & PageExtended;
 export type GetPageSecurityResponse = PageSecurity;
 
 export type GetPageContentsResponse = {
-    '@revision': string;
-    '@type': string;
-    '@title': string;
-    body: string[];
-    head: string;
-    tail: string;
-}
+  "@revision": string;
+  "@type": string;
+  "@title": string;
+  body: string[];
+  head: string;
+  tail: string;
+};
 
 export type GetPageSubPagesResponse = {
-    '@count': string;
-    '@href': string;
-    'page.subpage': PageBase | PageBase[];
-}
+  "@count": string;
+  "@href": string;
+  "page.subpage": PageBase | PageBase[];
+};
+
+export type GetPageDiffResponse = {
+  "@revision": string;
+  "@type": string;
+  p: {
+    "#text": string;
+    ins: string[];
+    del: string[];
+  };
+};
+
+export type GetPageFilesResponse = {
+  "@revision": string;
+  "@count": string;
+  "@href": string;
+  file: PageFile | PageFile[];
+};
 
 export type PageBase = {
-    '@id': string;
-    '@guid': string;
-    '@draft.state': string;
-    '@href': string;
-    '@deleted': string;
-    '@revision': string;
-    article: string;
-    'date.created': string;
-    'date.modified': string;
-    language: string;
-    namespace: string;
-    path: PagePath;
-    security: PageSecurity;
-    title: string;
-    'uri.ui': string;
-}
+  "@id": string;
+  "@guid": string;
+  "@draft.state": string;
+  "@href": string;
+  "@deleted": string;
+  "@revision": string;
+  article: string;
+  "date.created": string;
+  "date.modified": string;
+  language: string;
+  namespace: string;
+  path: PagePath;
+  security: PageSecurity;
+  title: string;
+  "uri.ui": string;
+};
 
 export type PageExtended = {
-    '@unpublish': string;
-    aliases: {
-        [key: string]: string;
-    },
-    comments: {
-        '@count': string;
-        '@href': string;
-    },
-    contents: {
-        '@type': string;
-        '@href': string;
-        '@etag': string;
-    },
-    'contents.alt': {
-        '@type': string;
-        '@href': string;
-    },
-    'date.edited': string;
-    description: string;
-    files: {
-        '@count': string;
-        '@href': string;
-        file: PageFile | PageFile[]
-    }
-    inbound: {
-        '@count': string;
-    },
-    'language.effective': string;
-    metrics: {
-        'metric.charcount': string;
-        'metric.views': string;
-    }
-    outbound: {
-        '@count': string;
-    }
-    'page.parent': GetPageResponse;
-    'page.redirectedfrom': string;
-    properties: {
-        '@count': string;
-        '@href': string;
-        property: Record<string, string>[];
-    },
-    rating: {
-        '@score': string;
-        '@count': string;
-        '@seated.score': string;
-        '@seated.count': string;
-        '@unseated.score': string;
-        '@unseated.count': string;
-        '@anonymous.score': string;
-        '@anonymous.count': string;
-    },
-    revisions: {
-        '@count': string;
-        '@href': string;
-    },
-    subpages: {
-        '@href': string;
-    },
-    summary: string;
-    tags: {
-        '@count': string;
-        '@href': string;
-        tag: PageTag | PageTag[]
-    },
-    timeuuid: string;
-    'user.author': ExpertUser;
-    'user.createdby': ExpertUser;
-}
-
+  "@unpublish": string;
+  aliases: {
+    [key: string]: string;
+  };
+  comments: {
+    "@count": string;
+    "@href": string;
+  };
+  contents: {
+    "@type": string;
+    "@href": string;
+    "@etag": string;
+  };
+  "contents.alt": {
+    "@type": string;
+    "@href": string;
+  };
+  "date.edited": string;
+  description: string;
+  files: {
+    "@count": string;
+    "@href": string;
+    file: PageFile | PageFile[];
+  };
+  inbound: {
+    "@count": string;
+  };
+  "language.effective": string;
+  metrics: {
+    "metric.charcount": string;
+    "metric.views": string;
+  };
+  outbound: {
+    "@count": string;
+  };
+  "page.parent": GetPageResponse;
+  "page.redirectedfrom": string;
+  properties: {
+    "@count": string;
+    "@href": string;
+    property: Record<string, string>[];
+  };
+  rating: {
+    "@score": string;
+    "@count": string;
+    "@seated.score": string;
+    "@seated.count": string;
+    "@unseated.score": string;
+    "@unseated.count": string;
+    "@anonymous.score": string;
+    "@anonymous.count": string;
+  };
+  revisions: {
+    "@count": string;
+    "@href": string;
+  };
+  subpages: {
+    "@href": string;
+  };
+  summary: string;
+  tags: {
+    "@count": string;
+    "@href": string;
+    tag: PageTag | PageTag[];
+  };
+  timeuuid: string;
+  "user.author": ExpertUser;
+  "user.createdby": ExpertUser;
+};
 
 export type PageFile = {
-    '@id': string;
-    '@revision': string;
-    '@res-id': string;
-    '@href': string;
-    '@res-is-head': string;
-    '@res-is-deleted': string;
-    '@res-rev-is-deleted': string;
-    '@res-contents-id': string;
-    'alt-text': string;
-    contents: object;
-    'contents.preview': any[];
-    'date.created': string;
-    'date.last-modified': string;
-    description: string;
-    filename: string;
-    revisions: object;
-    'user.createdBy': ExpertUser;
-}
+  "@id": string;
+  "@revision": string;
+  "@res-id": string;
+  "@href": string;
+  "@res-is-head": string;
+  "@res-is-deleted": string;
+  "@res-rev-is-deleted": string;
+  "@res-contents-id": string;
+  "alt-text": string;
+  contents: object;
+  "contents.preview": any[];
+  "date.created": string;
+  "date.last-modified": string;
+  description: string;
+  filename: string;
+  revisions: object;
+  "user.createdBy": ExpertUser;
+};
 
 export type PageTag = {
-    '@value': string;
-    '@id': string;
-    '@href': string;
-    title: string;
-    type: string;
-    url: string;
-}
+  "@value": string;
+  "@id": string;
+  "@href": string;
+  title: string;
+  type: string;
+  url: string;
+};
 
 export type PagePath = {
-    '@seo': string;
-    '#text': string;
-}
+  "@seo": string;
+  "#text": string;
+};
