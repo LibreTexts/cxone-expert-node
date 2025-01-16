c1f0486a60f76d1f6b763ffc7854c8372be7228b
AkhilTheBoss <akhileshwarshriram@gmail.com>
2025-01-15 16:32:50 -0800

    Added Groups GET API

---
+++ src/modules/groups.ts
@@ -0,0 +1,79 @@
+import {
+    BaseArgs,
+    ExpertGlobalOptions,
+    GetGroupsParams,
+    GetGroupsResponse,
+    GetGroupParams,
+    GetGroupResponse,
+    GetGroupUserParams,
+    GetGroupUserResponse
+  } from "../types";
+  import { getTld } from "../utils";
+  import Auth from "./auth";
+  import Requests from "./requests";
+
+export default class Groups {
+  private globals: ExpertGlobalOptions;
+  private _auth?: Auth;
+
+  constructor(args: ExpertGlobalOptions, auth?: Auth) {
+    this.globals = args;
+    this._auth = auth;
+  }
+
+  private parseGroupId(id: string | number) {
+    if (typeof id === "number") {
+      return id.toString();
+    }
+    return `=${encodeURIComponent(id)}`;
+  }
+
+  public async getGroups(
+      funcArgs: BaseArgs,
+      reqArgs?: GetGroupsParams
+  ) {
+      const tld = getTld(this.globals, funcArgs.tld);
+      const requests = new Requests(tld, funcArgs.auth);
+
+      const res = await requests.get<GetGroupsResponse>(`/groups`, {
+        params: {
+            ...reqArgs,
+        },
+      });
+      return res.data;
+    }
+
+  public async getGroup(
+      id: string | number,
+      funcArgs: BaseArgs,
+      reqArgs?: GetGroupParams
+    ) {
+      const groupId = this.parseGroupId(id);
+      const tld = getTld(this.globals, funcArgs.tld);
+      const requests = new Requests(tld, funcArgs.auth);
+  
+      const res = await requests.get<GetGroupResponse>(`/groups/${groupId}`, {
+        params: {
+          ...reqArgs,
+        },
+      });
+      return res.data;
+    }
+
+    public async getGroupUser(
+        id: string | number,
+        funcArgs: BaseArgs,
+        reqArgs?: GetGroupUserParams
+      ) {
+        const groupId = this.parseGroupId(id);
+        const tld = getTld(this.globals, funcArgs.tld);
+        const requests = new Requests(tld, funcArgs.auth);
+    
+        const res = await requests.get<GetGroupUserResponse>(`/groups/${groupId}/users`, {
+          params: {
+            ...reqArgs,
+          },
+        });
+        return res.data;
+      }
+}
